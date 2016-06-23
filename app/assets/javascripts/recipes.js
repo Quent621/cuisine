8c3a2875a492ca7a0bd94c3f1bfd8ed03e6f7e89
Quentin <quentin.varlet@gmail.com>
2016-06-23 16:56:28 +0200

    form and categories

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -20,10 +20,3 @@ app.controller('RecipeCtrl', ['$scope', '$http', function ($scope, $http) {
 }]);
 
 
-$(document).ready(function() {
-    $(".btn-pref .btn").click(function () {
-        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-        // $(".tab").addClass("active"); // instead of this do the below
-        $(this).removeClass("btn-default").addClass("btn-primary");
-    });
-});
