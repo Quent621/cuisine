cf1ac3eb44aaecc4f9790eba3190a93261272dce
Quentin <quentin.varlet@gmail.com>
2016-06-23 16:34:07 +0200

    change form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -63,6 +63,7 @@ class RecipesController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
@@ -70,6 +71,7 @@ class RecipesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def recipe_params
-    params.require(:recipes).permit(:name, :description, :duration, :photo, :user_id, :category_id, :ingredient_id, :difficulty, :preparation, :repose, :baking)
+    params.require(:recipe).permit(:name, :description, :duration, :photo, :user_id, :category_id, :ingredient_id, :difficulty, :preparation, :repose, :baking)
   end
+
 end
