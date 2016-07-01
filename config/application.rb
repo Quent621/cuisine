9c7a571ef6c310c65d0f9c43f47629736275a3ae
Quentin <quentin.varlet@gmail.com>
2016-07-01 10:34:51 +0200

    img heroku test

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Cuisine
   class Application < Rails::Application
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +24,8 @@ module Cuisine
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
